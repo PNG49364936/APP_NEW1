4291ec5519954a2a7ddd90616a34bf2fb8e75e2f
PURA <pura@MacBook-Air-de-PURA.local>
2023-03-24 16:02:38 +0100

    test

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,13 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-module APP_new1
+module APP_NEW1
   class Application < Rails::Application
     config.load_defaults 6.0
 
     # ... d'autres configurations ...
-
-    config.secret_key_base = ENV["714b38001c0632be187099c2accd3cfa7f2bad2494500065b5ce8b504930eb5657de6edb8bc1a7115d263275aa19398d325cf69659330aa2c902d86d93822e6a"] # ajouter cette ligne ici
+    
+    config.secret_key_base = ENV["694b40270b7187809ae27054531368b509b8ee1fe63830d2a4684258015803f09902cb7d5b22fc0ac1d8f52c03d3d8f332a59acd8b562d5586db64d60d3df72b"] # ajouter cette ligne ici
 
     # ... d'autres configurations ...
   end
